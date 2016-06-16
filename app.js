6cbc3050eb41734acc3c14478068846518b5cbab
Sergio Sanchez Robles <sergioj.sanchezr@gmail.com>
2016-06-16 08:49:15 -0500

    Archivo .env ignorado

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.use('/tarea1', tarea1);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
+  var err = new Error('Not Found' + res);
   err.status = 404;
   next(err);
 });
